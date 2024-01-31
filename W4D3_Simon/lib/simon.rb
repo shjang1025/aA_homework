369f952b8b2504821cf8b785ed4d5dda409ba259
shjang1025 <shjang1025@gmail.com>
2024-01-30 23:04:43 -0800

    define require sequence method

--- W4D3_Simon/lib/simon.rb
+++ W4D3_Simon/lib/simon.rb
@@ -10,57 +10,55 @@ class Simon
   end
 
   def play
+    # repeat until a condition becomes true
     until @game_over
-      take_turn
-      system("clear")
+      self.take_turn
     end
-
-    game_over_message
-    reset_game
+    
+    self.game_over_message
+    self.reset_game
+    
   end
 
   def take_turn
-    show_sequence
-    require_sequence
-
+    self.show_sequence
+    self.require_sequence
+    #things to be done if the condition is false
     unless @game_over
-      round_success_message
+      self.round_success_message
       @sequence_length += 1
     end
+    
   end
 
   def show_sequence
-    add_random_color
+    self.add_random_color
     @seq.each do |color|
       puts color
-      sleep 0.75
-      system("clear")
-      sleep 0.25
     end
   end
 
   def require_sequence
-    puts "Repeat the sequence by entering the first letter of each color on a new line."
     @seq.each do |color|
-      user_color = gets.chomp
-      if color[0] != user_color
+      user_input = gets.chomp
+      if user_input != color
         @game_over = true
         break
       end
     end
-    sleep 0.25
+
   end
 
   def add_random_color
-    @seq << COLORS.shuffle[0]
+    @seq << COLORS.sample
   end
 
   def round_success_message
-    "Round successful. Here's the next sequence:"
+    puts "Success!"
   end
 
   def game_over_message
-    puts "Game over! You made it #{@sequence_length - 1} rounds."
+    puts "Game Over"
   end
 
   def reset_game
